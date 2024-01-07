850409342c724961b9b963297328710ba750e505
seyman <ogunmusireseyi01@gmail.com>
2024-01-07 15:22:27 +0100

    learning django for backend

---
+++ trysite/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'seyman',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'opeyemi',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+# The about page sample
+def about(request):
+    return render(request, 'blog/about.html')
