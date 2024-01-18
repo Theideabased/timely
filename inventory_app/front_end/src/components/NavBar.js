0510d340053f20537555021f431ff46f40a0e8d5
seyman <ogunmusireseyi01@gmail.com>
2024-01-18 18:38:50 +0100

    frontend

---
+++ inventory_app/front_end/src/components/NavBar.js
@@ -0,0 +1,26 @@
+import react from "react"
+import {Navbar, Nav, Form, FormControl, Button, Badge, NavbarText } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+
+const NavBar = () => {
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand href="#home">Inventory Management App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar.nav">
+                <Nav className="mr-auto">
+                    <Badge className="mt-2" variant="primary">Products In stock</Badge>
+                </Nav>
+                <Form inline>
+                    <Link to="/addproduct" className="btn btn-primay btn sn mr-4">Add Product</Link>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button type="submit" variant="outline-primary">Search</Button>            
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+
+export default NavBar;
